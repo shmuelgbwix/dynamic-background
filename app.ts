e7797ec918c6b4c843f4ad75d8c87c23b37dd2a7
shmuelgbwix <shmuelg@wix.com>
2022-06-19 15:02:31 +0300

    handle cors, add orientaion

--- app.ts
+++ app.ts
@@ -1,15 +1,17 @@
 import "dotenv/config";
-import bodyParser from "body-parser";
 import express, { Request } from "express";
 import { ApiResponse } from "unsplash-js/dist/helpers/response";
 import { Random } from "unsplash-js/dist/methods/photos/types";
 import { unsplash } from "./api/unsplash";
-import { Site2backgrounds } from "./types";
+import { OrientationReq, RandomBackgroundReq, Site2backgrounds } from "./types";
+import cors from "cors";
+
 const app = express();
 
 let site2backgrounds: Site2backgrounds = {};
 
 const PORT = process.env.PORT || 3000;
+app.use(cors());
 app.use(express.json());
 
 setInterval(() => {
@@ -23,20 +25,24 @@ app.get("/", (req, res) => {
 
 app.get(
   "/background",
-  async (req: Request<any, any, any, { msId: string; query: string }>, res) => {
-    const { msId, query } = req.query;
+  async (req: Request<any, any, any, RandomBackgroundReq>, res) => {
+    let { msId, query, orientation } = req.query;
     if (site2backgrounds[msId]) {
       return res.send(site2backgrounds[msId]);
     }
-    const background = await getBackground(query);
+    const background = await getBackground(query, orientation);
     site2backgrounds[msId] = background!;
     return res.send(background);
   }
 );
 
-export const getBackground = async (query: string) => {
+export const getBackground = async (
+  query: string,
+  orientation: OrientationReq = "landscape"
+) => {
   const background = (await unsplash.photos.getRandom({
     query,
+    orientation,
   })) as ApiResponse<Random>;
   return background.response?.urls.raw;
 };
--- types.ts
+++ types.ts
@@ -1,1 +1,9 @@
 export type Site2backgrounds = Record<string, string>;
+
+export type RandomBackgroundReq = {
+  msId: string;
+  query: string;
+  orientation: OrientationReq;
+};
+
+export type OrientationReq = "landscape" | "portrait" | "squarish";
